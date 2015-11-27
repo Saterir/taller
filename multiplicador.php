c74bb2b0517a43d7e47b9a74651ef6f423d71d62
Nicolas <Nicolas@Nicolas-CG>
2015-11-27 19:27:55 -0200

    actualizado para llenar DB

--- multiplicador.php
+++ multiplicador.php
@@ -1,36 +1,13 @@
 <?php
-//fergetg
-function RandomString($length=10,$uc=TRUE,$n=TRUE,$sc=FALSE)
-{
-	$source = 'abcdefghijklmnopqrstuvwxyz';
-	if($uc==1) $source .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	if($n==1) $source .= '1234567890';
-	if($sc==1) $source .= '|@#~%=^*+-_';
-	if($length>0){
-		$rstr = "";
-		$source = str_split($source,1);
-		for($i=1; $i<=$length; $i++){
-			mt_srand((double)microtime() * 1000000);
-			$num = mt_rand(1,count($source));
-			$rstr .= $source[$num-1];
+	$contrasena='1234';
+	for ($i = 0; $i < 1000000; $i++) {
+		$usuario=$i;
+		$mysqli = new mysqli($_SERVER["host"], $_SERVER["user"], $_SERVER["pass"], $_SERVER["dbh"]);
+		if (!$mysqli->multi_query("SET @p1='$usuario'; SET @p2='$contrasena'; 
+					SET @p3='0'; SET @p4='1'; SET @p5='';
+					CALL crear_usuario(@p1,@p2,@p3,@p4,@p5);")) {
+			echo "Falló la llamada: (" . $mysqli->errno . ") " . $mysqli->error;
 		}
-
 	}
-	return $rstr;
-}
-for ($i = 0; $i < 1500000; $i++) {
-	$usuario=RandomString(7,TRUE,TRUE,TRUE);
-	$contrasena=RandomString(15,TRUE,TRUE,TRUE);
-
-	$mysqli = new mysqli($_SERVER["host"], $_SERVER["user"], $_SERVER["pass"], $_SERVER["dbh"]);
-	if (!$mysqli->multi_query("SET @p1='$usuario'; SET @p2='$contrasena'; CALL insertarusuario(@p1,@p2);")) {
-		echo "Falló la llamada: (" . $mysqli->errno . ") " . $mysqli->error;
-	}else{
-		echo '
-			El usuario ha sido registrado correctamente.
-			<br>
-			<a href="newfile.php">volver</a>
-			';
-	}
-}
-?>
+?>	
+	
